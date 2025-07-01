2ad77980831c9ca9dcc4d68e0ceb07340c5689c6
dgpetrie <dpetrie@sipez.com>
2025-07-01 12:21:03 -0400

    Removed exampels from overivew

--- examples/simple-vcon.pp
+++
@@ -1,9 +0,0 @@
-{
-  "vcon" : 0.0.1,
-  "text" : "Really long lineeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeh",
-  "subject" : "test kramdown include to appendix"
-}
-
